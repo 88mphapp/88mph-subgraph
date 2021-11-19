2ef4cfa1d65f5ae820f9bff1d98245afd6818579
ZeframLou <zeframlou@gmail.com>
2021-11-18 23:04:32 -0800

    fix(mphminter): Fix MPHMinter::handleESetParamUint not getting pool when it's not initialized

--- src/MPHMinter.ts
+++ src/MPHMinter.ts
@@ -1,14 +1,11 @@
 import { ESetParamUint } from "../generated/MPHMinter/MPHMinter";
 import { ERC20 } from "../generated/MPHMinter/ERC20";
 import { DPool } from "../generated/schema";
-import { getTokenDecimals, keccak256, normalize } from "./utils";
+import { getPool, getTokenDecimals, keccak256, normalize } from "./utils";
 import { Address } from "@graphprotocol/graph-ts";
 
 export function handleESetParamUint(event: ESetParamUint): void {
-  let pool = DPool.load(event.params.pool.toHex());
-  if (pool == null) {
-    return;
-  }
+  let pool = getPool(event.params.pool.toHex());
   let stablecoinDecimals: number = getTokenDecimals(
     Address.fromString(pool.stablecoin)
   );
