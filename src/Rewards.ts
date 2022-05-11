6e6880992e14c2db4ea6945887dc82c431591cd6
0xSzeth <0xSzeth@gmail.com>
2022-05-11 14:49:16 -0600

    Revert "Bump dependencies. Fix erorrs."
    This reverts commit 96975e11a3836f0db24dbd372adca38bc9964dde.

--- src/Rewards.ts
+++ src/Rewards.ts
@@ -4,61 +4,55 @@ import {
   Staked as EStaked,
   Withdrawn as EWithdrawn,
   RewardPaid as ERewardPaid
-} from "../generated/Rewards/Rewards";
-import { ZERO_DEC, normalize, getMPH, getMPHHolder } from "./utils";
+} from '../generated/Rewards/Rewards'
+import { ZERO_DEC, normalize, getMPH, getMPHHolder } from './utils'
 
 export function handleRewardAdded(event: ERewardAdded): void {
-  let mph = getMPH();
-  let rewards = Rewards.bind(event.address);
+  let mph = getMPH()
+  let rewards = Rewards.bind(event.address)
 
-  mph.rewardPerSecond = normalize(rewards.rewardRate());
-  let totalStakedMPHBalance = normalize(rewards.totalSupply());
-  mph.rewardPerMPHPerSecond = totalStakedMPHBalance.equals(ZERO_DEC)
-    ? ZERO_DEC
-    : mph.rewardPerSecond.div(totalStakedMPHBalance);
-  mph.totalHistoricalReward = mph.totalHistoricalReward.plus(
-    normalize(event.params.reward)
-  );
+  mph.rewardPerSecond = normalize(rewards.rewardRate())
+  let totalStakedMPHBalance = normalize(rewards.totalSupply())
+  mph.rewardPerMPHPerSecond = totalStakedMPHBalance.equals(ZERO_DEC) ?
+    ZERO_DEC :
+    mph.rewardPerSecond.div(totalStakedMPHBalance)
+  mph.totalHistoricalReward = mph.totalHistoricalReward.plus(normalize(event.params.reward))
 
-  mph.save();
+  mph.save()
 }
 
 export function handleStaked(event: EStaked): void {
-  let mph = getMPH();
-  let rewards = Rewards.bind(event.address);
+  let mph = getMPH()
+  let rewards = Rewards.bind(event.address)
 
-  let stakeAmount = normalize(event.params.amount);
-  let totalStakedMPHBalance = normalize(rewards.totalSupply());
-  mph.rewardPerSecond = normalize(rewards.rewardRate());
-  mph.rewardPerMPHPerSecond = mph.rewardPerSecond.div(totalStakedMPHBalance);
+  let stakeAmount = normalize(event.params.amount)
+  let totalStakedMPHBalance = normalize(rewards.totalSupply())
+  mph.rewardPerSecond = normalize(rewards.rewardRate())
+  mph.rewardPerMPHPerSecond = mph.rewardPerSecond.div(totalStakedMPHBalance)
 
-  mph.save();
+  mph.save()
 }
 
 export function handleWithdrawn(event: EWithdrawn): void {
-  let mph = getMPH();
-  let rewards = Rewards.bind(event.address);
+  let mph = getMPH()
+  let rewards = Rewards.bind(event.address)
 
-  let totalStakedMPHBalance = normalize(rewards.totalSupply());
-  mph.rewardPerSecond = normalize(rewards.rewardRate());
-  mph.rewardPerMPHPerSecond = totalStakedMPHBalance.equals(ZERO_DEC)
-    ? ZERO_DEC
-    : mph.rewardPerSecond.div(totalStakedMPHBalance);
+  let totalStakedMPHBalance = normalize(rewards.totalSupply())
+  mph.rewardPerSecond = normalize(rewards.rewardRate())
+  mph.rewardPerMPHPerSecond = totalStakedMPHBalance.equals(ZERO_DEC) ?
+    ZERO_DEC :
+    mph.rewardPerSecond.div(totalStakedMPHBalance)
 
-  mph.save();
+  mph.save()
 }
 
 export function handleRewardPaid(event: ERewardPaid): void {
-  let mph = getMPH();
-  let mphHolder = getMPHHolder(event.params.user);
+  let mph = getMPH()
+  let mphHolder = getMPHHolder(event.params.user)
 
-  if (mphHolder) {
-    let rewardAmount = normalize(event.params.reward);
-    mphHolder.totalHistoricalReward = mphHolder.totalHistoricalReward.plus(
-      rewardAmount
-    );
+  let rewardAmount = normalize(event.params.reward)
+  mphHolder.totalHistoricalReward = mphHolder.totalHistoricalReward.plus(rewardAmount)
 
-    mphHolder.save();
-    mph.save();
-  }
+  mphHolder.save()
+  mph.save()
 }
